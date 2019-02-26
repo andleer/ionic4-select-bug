b256392f7ead0df8a63217b1a951f0f9c6a83f4c
Andrew Robinson <andrew@binaryocean.com>
2019-02-25 17:19:29 -0800

    load elements after selected

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ElementRef, ViewChild, NgZone, ApplicationRef, ChangeDetectorRef } from '@angular/core';
-import { IWidget } from './widget.model';
 import { IonSelect } from '@ionic/angular';
 
 import 'zone.js/dist/zone';
@@ -14,40 +13,30 @@ export class HomePage implements OnInit {
   @ViewChild('widgetSelect2') widgetSelect2: IonSelect;
   @ViewChild('widgetSelect3') widgetSelect3: IonSelect;
 
-  public pseudoApiSource: IWidget[];
+  public pseudoApiSource = [
+    'Widget A',
+    'Widget B',
+    'Widget C',
+    'Widget D',
+    'Widget E',
+    'Widget F',
+  ];
 
-  public widgets1: IWidget[];
-  public widgets2: IWidget[];
-  public widgets3: IWidget[];
+  public widgets1: string[];
+  public widgets2: string[];
+  public widgets3: string[];
 
-  public selectedWidgetId: number;
+  public selectedWidget = this.pseudoApiSource[4];
 
   // always shows
   public readonly delay1 = 10;
-
-  // 55 - 70ms seem to cause the bug on my
-  // X1 Carbon i7-7600U CPU @ 2.80 - 2.90 GHz
-  // at 65 on my device this seems intermittent
-  // I don't know if this is event driven
-  // or just a timing side effect
-  public readonly delay2 = 65;
-
-  // never shows. bug!!!
-  public readonly delay3 = 200;
+  public readonly delay2 = 50;
+  public readonly delay3 = 100;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.pseudoApiSource = [
-      { id: 1, name: 'Widget A', isDefault: false, },
-      { id: 2, name: 'Widget B', isDefault: false, },
-      { id: 3, name: 'Widget C', isDefault: false, },
-      { id: 4, name: 'Widget D', isDefault: true, },
-      { id: 5, name: 'Widget E', isDefault: false, },
-      { id: 6, name: 'Widget F', isDefault: false, },
-    ];
-
-    console.log();
+      console.log();
     console.log('widget starting...');
 
     setTimeout(() => {
@@ -63,14 +52,6 @@ export class HomePage implements OnInit {
     setTimeout(() => {
       this.widgets3 = this.pseudoApiSource;
       console.log('widget 3 loaded');
-      setTimeout(() => {
-        // setting the value here after a delay of about 50ms or more
-        // after the "api" call completes
-        // causes the selected text to appear correctly.
-        // remove the delay and just set the value at the end here
-        // and the selected text will fail to display.
-        this.selectedWidgetId = 4;
-      }, 100);
     }, this.delay3);
   }
 
